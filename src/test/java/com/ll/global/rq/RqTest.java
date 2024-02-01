1833b65afb052633068cd1ea28c7ccc08dd79be2
seoyeon <tus0926@naver.com>
2024-02-01 17:09:35 +0900

    11- 4. 질문 (실패한 테스트) - Rq: getParameterOr

--- src/test/java/com/ll/global/rq/RqTest.java
+++ src/test/java/com/ll/global/rq/RqTest.java
@@ -38,4 +38,14 @@ public class RqTest {
         assertThat(rq.getParameter("id")).isEqualTo("4");
         assertThat(rq.getParameter("이름")).isEqualTo("Paul");
     }
+
+    @Test
+    @DisplayName("getParameterOr")
+    void t5() {
+        final Rq rq = new Rq("삭제?id=4&이름=Paul");
+
+        String age = rq.getParameter("나이", "100");
+
+        assertThat(age).isEqualTo(100);
+    }
 }
