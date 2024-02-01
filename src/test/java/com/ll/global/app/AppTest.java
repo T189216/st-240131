0e290954bff9a04b6f9a3d27ddcc9632bae3192c
seoyeon <tus0926@naver.com>
2024-02-01 17:48:48 +0900

    13- 1. 질문 (실패한 테스트) - 수정

--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -166,4 +166,28 @@ public class AppTest {
                 .doesNotContain("2 / 홍길동 / 과거에 집착하지 마라.")
                 .contains("1 / 작자미상 / 현재를 사랑하라.");
     }
+
+    @Test
+    @DisplayName("수정")
+    void t10() {
+        final String out = run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                홍길동
+                수정?id=2
+                성공은 영원하지 않고, 실패는 치명적이지 않다.
+                마이크 디트카
+                목록
+                """);
+
+        assertThat(out)
+                .contains("번호 / 작가 / 명언")
+                .contains("------------------")
+                .doesNotContain("2 / 홍길동 / 과거에 집착하지 마라.")
+                .contains("2 / 마이크 디트카 / 성공은 영원하지 않고, 실패는 치명적이지 않다.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+    }
 }
