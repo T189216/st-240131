23b2de0b660d40ef06a61e0af54ecad5d92769bf
seoyeon <tus0926@naver.com>
2024-02-01 14:46:38 +0900

    9-4. 응답 (성공한 테스트) - 삭제 2

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -62,6 +62,12 @@ public class App {
 
                     System.out.println("1번 명언이 삭제되었습니다.");
                 }
+                case "삭제?id=2" -> {
+                    quotationList
+                            .removeIf(quotation -> quotation.getId() == 2);
+
+                    System.out.println("2번 명언이 삭제되었습니다.");
+                }
             }
         }
     }
